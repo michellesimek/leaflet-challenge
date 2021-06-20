907490ccf9fe4505698725c946f57ef6b1c58724
Michelle Simek <michellesimek@michelles-mbp.attlocal.net>
2021-06-19 22:26:51 -0500

    revised script so markers appear

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -23,7 +23,7 @@ function createFeatures(earthquakeData) {
 
 function createMap(earthquakes) {
 // create LayerGroup for Earthquake data
-    let earthquakeLayer = new L.layerGroup();
+    // let earthquakeLayer = new L.layerGroup();
 
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -47,14 +47,14 @@ function createMap(earthquakes) {
     };
 
     var overlayMaps = {
-        Earthquakes: earthquakeLayer
+        Earthquakes: earthquakes
     };
 
     // Create a new map
     var myMap = L.map("map", {
         center: [37.09, -95.71],
         zoom: 2,
-        layers: [streetmap]
+        layers: [streetmap, earthquakes]
     });
     
     // Create a layer control containing our baseMaps
